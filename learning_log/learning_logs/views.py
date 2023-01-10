5d5b2c488399a7fa0c45e865c03af1c1d9bd2f69
Sasha Osin <sashaosin364@gmail.com>
2023-01-10 21:48:03 +0700

    fix bags

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,40 +1,46 @@
 from django.shortcuts import render,redirect
 
+from django.contrib.auth.decorators import login_required
 from .models import Topic,Entry
 from .forms import TopicForm,EntryForm
+from django.http import Http404
 def index(request):
     return render(request,'learning_logs/index.html')
-
+@login_required
 def topics(request):
-    topics = Topic.objects.order_by('data_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('data_added')
     context = {'topics':topics}
     return render(request,'learning_logs/topics.html',context=context)
-
+@login_required
 def topic(request,topic_id):
     topic = Topic.objects.get(id = topic_id)
-    entries = topic.entry_set.order_by('-data_added')
+    if topic.owner != request.user:
+        raise Http404
+    entries = topic.entry_set.order_by('data_added')
     context = {'topic':topic,'entries':entries}
     return render(request,'learning_logs/topic.html',context=context)
-
+@login_required
 def new_topic(request):
     if request.method != 'POST':
         form = TopicForm()
     else:
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect('learning_logs:topics')
 
     context = {'form':form}
     return render(request,'learning_logs/new_topic.html',context)
-
+@login_required
 def new_entry(request,topic_id):
     topic = Topic.objects.get(id=topic_id)
     if request.method != 'POST':
         form = EntryForm
     else:
         form = EntryForm(data=request.POST)
-        if form.is_valid():
+        if form.is_valid() and topic.owner == request.user:
             new_entry = form.save(commit=False)
             new_entry.topic = topic
             new_entry.save()
@@ -42,10 +48,12 @@ def new_entry(request,topic_id):
 
     context = {'topic':topic,'form':form}
     return render(request,'learning_logs/new_entry.html',context=context)
-
+@login_required
 def edit_entry(request,entry_id):
     entry = Entry.objects.get(id=entry_id)
-    topic=entry.topic
+    topic = entry.topic
+    if topic.owner != request.user:
+        raise Http404
 
     if request != 'POST':
         form = EntryForm(instance=entry)
@@ -56,3 +64,7 @@ def edit_entry(request,entry_id):
             return redirect('learning_logs:topic',topic_id=topic.id)
     context = {'entry':entry,'topic':topic,'form':form}
     return render(request,'learning_logs/edit_entry.html',context=context)
+
+r = 'fafa'
+
+r.spl
